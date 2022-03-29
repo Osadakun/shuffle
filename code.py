39c6c4eae62fbf0cedcc863bf509431feccc3fb8
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-03-29 15:16:57 +0900

    学籍番号載せるのはまずい

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 import random
-members = {1915053:"小野紀輝", 1916984:"鈴木智紀", 1102646:"池田了哉", 1901805:"北川裕基", 1139952:"中屋飛人", 1914801:"笹野章彦", 1102691:"石原泰知", 1140347:"近川健太"}
 
 def shuffleDict(d):
   keys = list(d.keys())
